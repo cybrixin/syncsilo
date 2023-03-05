6d4c68b08244a0757d70e22bedd8c11571288011
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2023-03-06 03:48:17 +0530

    Add: the new folder component to render on screen.

---
+++ src/components/Folder/Folder.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+export default function Folder({ folder }) {
+  return (
+    <Button
+        to={{
+            pathname: `/folder/${folder.id}`,
+            state: { folder: folder },
+        }}
+        variant="outline-dark"
+        className="text-truncate w-100"
+        as={Link}>
+        <i className="fas fa-folder mr-2"></i>
+        {folder.name}
+    </Button>
+  )
+}
