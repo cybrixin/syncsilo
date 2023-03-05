23d884bdb33a34b2aa2d4f60e59b2cfff791744b
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2023-03-06 03:43:07 +0530

    Dummy Component.
    Will be updated in the next update

---
+++ src/components/AddFolderButton/AddFolderButton.jsx
@@ -0,0 +1,16 @@
+
+import React, { useState } from "react"
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+import { useApp } from "@/contexts/AppContext";
+import { useAuth } from "@/contexts/AuthContext";
+import { ROOT_FOLDER } from "@/hooks/useFolder";
+
+export default function AddFolderButton() {
+  return (
+    <div>AddFolderButton</div>
+  )
+}
