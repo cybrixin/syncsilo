88b0beef3907fd8a9d3e2619a24625e29704f9e4
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2023-03-07 18:05:06 +0530

    Auth pages updated with notice

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,6 +7,7 @@ import React, { useRef, useState, useEffect } from "react"
 import CenteredContainer from "@/components/CenteredContainer"
 
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
 import Card from 'react-bootstrap/Card'
@@ -57,10 +58,20 @@ export default function Login() {
 	}
 
 	async function handleSSO (evt) {
+
+		evt.preventDefault();
+        resetAllState();
+        setLoading(true)
+		
 		try {
+			const provider = evt.target.dataset.provider;
+
+			// await sso(provider);
 
 		}catch ( err ) {
 
+		}finally {
+			setLoading(!true)
 		}
 	}
 
@@ -143,56 +154,65 @@ export default function Login() {
     
 
     return (
-		<CenteredContainer style={{'--max-width': '800px'}}>
-			<Card className={styles.card}>
-				<Card.Body>
-					<h4 className={`m-0 mb-2 ${styles.intro}`}>Hi! {greet()}{' '}<span className='emoji'>{greetEmoji()}</span></h4>
-					<p className={`m-0 mb-2 ${styles.intro}`} style={{'--font-family': 'Montserrat', '--font-weight': '400',}}>We are glad to have you back. Sign in with your credentials and let hop on the journey 🤝🏻!</p>
-					<p className={`m-0 mb-2 ${styles.intro} ${styles.danger}`}>All <i className={`far fa-asterisk ${styles.asterisk}`}></i> fields are required.</p>
-					<hr/>
-					<Form onSubmit={handleSubmit} noValidate validated={validated}>
-						<Form.Group id="email" as={Col}>
-							<Form.Label className={styles.label}>Email&nbsp;<i className={`far fa-asterisk ${styles.asterisk}`}></i></Form.Label>
-							<InputGroup hasValidation >
-								<InputGroup.Text id="inputGroupEmailPrepend" className={styles['input-group-text-prepend']}><i className='fas fa-at'></i></InputGroup.Text>
-								<Form.Control type="email" ref={emailRef} disabled={loading} required isValid={validated === true && emailError === false} placeholder="johndoe@example.com" />
-								{ emailError !== false ? (<Form.Control.Feedback type="invalid">
-									{emailError}
-								</Form.Control.Feedback>) : <></> }
-							</InputGroup>
-						</Form.Group>
-						<Form.Group id="password" as={Col} className={styles.gap}>
-							<Form.Label className={styles.label}>Password&nbsp;<i className={`far fa-asterisk ${styles.asterisk}`}></i></Form.Label>
-							<InputGroup hasValidation>
-								<InputGroup.Text id="inputGroupPasswordPrepend" className={styles['input-group-text-prepend']}><i className='fas fa-user-secret'></i></InputGroup.Text>
-								<Form.Control type={slash ? "text" : "password"} className={styles['input-group-text-prepend']} ref={passwordRef} disabled={loading} required isValid={validated === true && passwordError === false} placeholder="Enter your password" />
-								<InputGroup.Text title="Show password" onClick={() => setSlash( prev => !prev)} className={styles['password-eye-button']}><i className={`fas fa-eye${slash ? '-slash' : ""}`}></i></InputGroup.Text>
-								{ passwordError !== false ? (<Form.Control.Feedback type="invalid">
-									{passwordError}
-								</Form.Control.Feedback>) : <></> }
-							</InputGroup>
-						</Form.Group>
-						{error && (<><hr/> <p className={`m-0 mt-2 mb-2 ${styles.intro} ${styles.danger}`} id={styles[!success ? `auth-error` : `auth-success`]} style={{'--font-family': 'Montserrat', '--font-weight': '500',}}>{error}</p></>)}
+		<>
+			<Alert variant="warning">
+				<Alert.Heading>Social Sign In is currently under development</Alert.Heading>
+				<hr/>
+				<p>Hi 👋🏻, please note that currently sign in in with Google/Facebook/Twitter is under development. There are various reasons like Facebook will take 24 hrs to review the App Policy, website etc, while Twitter is reviewing my dev account based on their new Terms & Conditions. I will update these as soon as they are available.</p>
+				<p>On the other hand, I need a little bit more time to configure Google Linking which may take a bit more time.</p>
+				<p>Your patience is appreciated.</p>
+			</Alert>
+			<CenteredContainer style={{'--max-width': '800px'}}>
+				<Card className={styles.card}>
+					<Card.Body>
+						<h4 className={`m-0 mb-2 ${styles.intro}`}>Hi! {greet()}{' '}<span className='emoji'>{greetEmoji()}</span></h4>
+						<p className={`m-0 mb-2 ${styles.intro}`} style={{'--font-family': 'Montserrat', '--font-weight': '400',}}>We are glad to have you back. Sign in with your credentials and let hop on the journey 🤝🏻!</p>
+						<p className={`m-0 mb-2 ${styles.intro} ${styles.danger}`}>All <i className={`far fa-asterisk ${styles.asterisk}`}></i> fields are required.</p>
+						<hr/>
+						<Form onSubmit={handleSubmit} noValidate validated={validated}>
+							<Form.Group id="email" as={Col}>
+								<Form.Label className={styles.label}>Email&nbsp;<i className={`far fa-asterisk ${styles.asterisk}`}></i></Form.Label>
+								<InputGroup hasValidation >
+									<InputGroup.Text id="inputGroupEmailPrepend" className={styles['input-group-text-prepend']}><i className='fas fa-at'></i></InputGroup.Text>
+									<Form.Control type="email" ref={emailRef} disabled={loading} required isValid={validated === true && emailError === false} placeholder="johndoe@example.com" />
+									{ emailError !== false ? (<Form.Control.Feedback type="invalid">
+										{emailError}
+									</Form.Control.Feedback>) : <></> }
+								</InputGroup>
+							</Form.Group>
+							<Form.Group id="password" as={Col} className={styles.gap}>
+								<Form.Label className={styles.label}>Password&nbsp;<i className={`far fa-asterisk ${styles.asterisk}`}></i></Form.Label>
+								<InputGroup hasValidation>
+									<InputGroup.Text id="inputGroupPasswordPrepend" className={styles['input-group-text-prepend']}><i className='fas fa-user-secret'></i></InputGroup.Text>
+									<Form.Control type={slash ? "text" : "password"} className={styles['input-group-text-prepend']} ref={passwordRef} disabled={loading} required isValid={validated === true && passwordError === false} placeholder="Enter your password" />
+									<InputGroup.Text title="Show password" onClick={() => setSlash( prev => !prev)} className={styles['password-eye-button']}><i className={`fas fa-eye${slash ? '-slash' : ""}`}></i></InputGroup.Text>
+									{ passwordError !== false ? (<Form.Control.Feedback type="invalid">
+										{passwordError}
+									</Form.Control.Feedback>) : <></> }
+								</InputGroup>
+							</Form.Group>
+							{error && (<><hr/> <p className={`m-0 mt-2 mb-2 ${styles.intro} ${styles.danger}`} id={styles[!success ? `auth-error` : `auth-success`]} style={{'--font-family': 'Montserrat', '--font-weight': '500',}}>{error}</p></>)}
+							<hr/>
+							{ !loading ? <div className="w-100 mt-3">
+								<Link to="/forgot-password">Forgot Password?</Link>
+							</div> : <></> }
+							<Button disabled={loading} variant="outline-primary" className={`${styles.button} mt-2`} type="submit">Sign In</Button>
+						</Form>
+						<div className="w-100 border border-dark mt-3 h-0 mb-3" id={styles['or']} data-content="OR"></div>
+						<Stack direction="horizontal" gap={3} className="me-auto w-100">
+							<Button className="w-50 text-start vertical-middle" variant="outline-dark" style={{'--bs-btn-hover-border-color': '#db4437', '--bs-btn-hover-bg': '#db4437'}} onClick={handleSSO} data-provider="google" disabled={true}><i className="fab fa-google"></i>&nbsp;Sign in with Google</Button>
+							<div className="vr" />
+							<Button className="w-50 text-start vertical-middle" variant="outline-dark" style={{'--bs-btn-hover-border-color': '#3c5a99', '--bs-btn-hover-bg': '#3c5a99'}} onClick={handleSSO} data-provider="facebook" disabled={true}><i className="fab fa-facebook"></i>&nbsp;Sign in with Facebook</Button>
+							<div className="vr" />
+							<Button className="w-50 text-start vertical-middle" variant="outline-dark" style={{'--bs-btn-hover-border-color': '#1da1f2', '--bs-btn-hover-bg': '#1da1f2'}} onClick={handleSSO} data-provider="twitter" disabled={true}><i className="fab fa-twitter"></i>&nbsp;Sign in with Twitter</Button>
+						</Stack>
 						<hr/>
 						{ !loading ? <div className="w-100 mt-3">
-							<Link to="/forgot-password">Forgot Password?</Link>
-						</div> : <></> }
-						<Button disabled={loading} variant="outline-primary" className={`${styles.button} mt-2`} type="submit">Sign In</Button>
-					</Form>
-					<div className="w-100 border border-dark mt-3 h-0 mb-3" id={styles['or']} data-content="OR"></div>
-					<Stack direction="horizontal" gap={3} className="me-auto w-100">
-						<Button className="w-50 text-start vertical-middle" variant="outline-dark" style={{'--bs-btn-hover-border-color': '#db4437', '--bs-btn-hover-bg': '#db4437'}} onClick={handleSSO} data-provider="facebook"><i className="fab fa-google"></i>&nbsp;Sign up with Google</Button>
-						<div className="vr" />
-						<Button className="w-50 text-start vertical-middle" variant="outline-dark" style={{'--bs-btn-hover-border-color': '#3c5a99', '--bs-btn-hover-bg': '#3c5a99'}} onClick={handleSSO} data-provider="facebook"><i className="fab fa-facebook"></i>&nbsp;Sign up with Facebook</Button>
-						<div className="vr" />
-						<Button className="w-50 text-start vertical-middle" variant="outline-dark" style={{'--bs-btn-hover-border-color': '#1da1f2', '--bs-btn-hover-bg': '#1da1f2'}} onClick={handleSSO} data-provider="twitter"><i className="fab fa-twitter"></i>&nbsp;Sign up with Twitter</Button>
-					</Stack>
-					<hr/>
-					{ !loading ? <div className="w-100 mt-3">
-						Need an account? <Link to="/signup">Sign Up</Link>
-					</div> : <></>}
-				</Card.Body>
-			</Card>
-		</CenteredContainer>
+							Need an account? <Link to="/signup">Sign Up</Link>
+						</div> : <></>}
+					</Card.Body>
+				</Card>
+			</CenteredContainer>
+		</>
     )
 }
